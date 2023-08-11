bf47e603761cef0cec8598191d92c2b7b448c206
Davis Shriver <19dshriver@gmail.com>
2023-08-11 11:10:28 -0500

    Updated application models

--- models/applicationModel.go
+++ models/applicationModel.go
@@ -3,13 +3,40 @@ package models
 import "time"
 
 type Application struct {
-	ApplicationID int       `gorm:"primaryKey;autoIncrement"`
-	PostID        int       `json:"PostID" validate:"required"`
-	UserID        int       `json:"description" validate:"required,min=2,max=300"`
-	Experience    string    `json:"requirements" validate:"required,min=2,max=300"`
-	CreatedBy     string    `json:"created_by" validate:"required"`
-	Address       string    `json:"location" validate:"required"`
-	Wage          int       `json:"wage" validate:"required"`
-	CreatedAt     time.Time `json:"created_at"`
-	UpdatedAt     time.Time `json:"expires_at"`
+	ApplicationID int              `gorm:"primaryKey;autoIncrement"`
+	FirstName     string           `json:"first_name" validate:"required,min=2,max=100"`
+	LastName      string           `json:"first_name" validate:"requiredmin=2,max=100"`
+	Email         string           `json:"email" validate:"email,required"`
+	Phone         string           `json:"phone" validate:"required,min=1,max=10"`
+	Address       string           `json:"address" validate:"required"`
+	City          string           `json:"city" validate:"required"`
+	State         string           `json:"state" validate:"required"`
+	PostalCode    string           `json:"postal_code" validate:"required"`
+	CoverLetter   string           `json:"cover_letter"`
+	ResumeURL     string           `json:"resume_url" validate:"required"`
+	LinkedInURL   string           `json:"linkedin_url"`
+	PortfolioURL  string           `json:"portfolio_url"`
+	References    []string         `json:"references"`
+	DesiredSalary float64          `json:"desired_salary"`
+	Availability  string           `json:"availability"`
+	Education     []EducationInfo  `json:"education"`
+	WorkHistory   []WorkExperience `json:"work_history"`
+	CreatedAt     time.Time        `json:"created_at"`
+	UpdatedAt     time.Time        `json:"expires_at"`
+}
+
+type EducationInfo struct {
+	Degree   string `json:"degree"`
+	School   string `json:"school"`
+	Location string `json:"location"`
+	GradYear int    `json:"grad_year"`
+}
+
+type WorkExperience struct {
+	Position         string   `json:"position"`
+	Company          string   `json:"company"`
+	Location         string   `json:"location"`
+	StartYear        int      `json:"start_year"`
+	EndYear          int      `json:"end_year"`
+	Responsibilities []string `json:"responsibilities"`
 }
