88b316120d4a81a3efeed8fe6db6721254c9c938
19dshriver <19dshriver@gmail.com>
2023-08-14 15:16:40 -0500

    For debugging

--- models/applicationModel.go
+++ models/applicationModel.go
@@ -3,28 +3,28 @@ package models
 import "time"
 
 type Application struct {
-	ApplicationID int              `gorm:"primaryKey;autoIncrement"`
-	UserID        int              `json:"user_id"`
-    PostID        int              `json:"post_id"`
-	FirstName     string           `json:"first_name" validate:"required,min=2,max=100"`
-	LastName      string           `json:"first_name" validate:"requiredmin=2,max=100"`
-	Email         string           `json:"email" validate:"email,required"`
-	Phone         string           `json:"phone" validate:"required,min=1,max=10"`
-	Address       string           `json:"address" validate:"required"`
-	City          string           `json:"city" validate:"required"`
-	State         string           `json:"state" validate:"required"`
-	PostalCode    string           `json:"postal_code" validate:"required"`
-	CoverLetter   string           `json:"cover_letter"`
-	ResumeURL     string           `json:"resume_url" validate:"required"`
-	LinkedInURL   string           `json:"linkedin_url"`
-	PortfolioURL  string           `json:"portfolio_url"`
-	References    []string         `json:"references"`  
-	DesiredSalary float64          `json:"desired_salary" validate:"required"`  
-	Availability  string           `json:"availability" validate:"required"`
-	Education     []EducationInfo  `json:"education"`
-	WorkHistory   []WorkExperience `json:"work_history"`
-	CreatedAt     time.Time        `json:"created_at"`
-	UpdatedAt     time.Time        `json:"expires_at"`
+	ApplicationID int      `gorm:"primaryKey;autoIncrement"`
+	UserID        int      `json:"user_id"`
+	PostID        int      `json:"post_id"`
+	FirstName     string   `json:"first_name" validate:"required,min=2,max=100"`
+	LastName      string   `json:"last_name" validate:"requiredmin=2,max=100"`
+	Email         string   `json:"email" validate:"email,required"`
+	Phone         string   `json:"phone" validate:"required,min=1,max=10"`
+	Address       string   `json:"address" validate:"required"`
+	City          string   `json:"city" validate:"required"`
+	State         string   `json:"state" validate:"required"`
+	PostalCode    string   `json:"postal_code" validate:"required"`
+	CoverLetter   string   `json:"cover_letter"`
+	ResumeURL     string   `json:"resume_url" validate:"required"`
+	LinkedInURL   string   `json:"linkedin_url"`
+	PortfolioURL  string   `json:"portfolio_url"`
+	References    []string `json:"references"`
+	DesiredSalary float64  `json:"desired_salary" validate:"required"`
+	Availability  string   `json:"availability" validate:"required"`
+	//Education     []EducationInfo  `json:"education"`
+	//WorkHistory   []WorkExperience `json:"work_history"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"expires_at"`
 }
 
 type EducationInfo struct {
